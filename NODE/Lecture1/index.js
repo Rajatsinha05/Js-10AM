df867fb58d076bdb9d64daf94ae18fce1003157e
Rajat sinha <rajatsinha5467@gmail.com>
2024-04-30 11:01:38 +0530

    js

---
+++ NODE/Lecture1/index.js
@@ -0,0 +1,56 @@
+const FunctionName = () => {
+
+
+}
+
+
+// let name = "node"
+
+// console.log(name);
+
+// GET()   returns data 
+// POST()  data 
+// PUT()   id/data
+// PATCH() id/data
+// DELETE() id
+
+
+// 
+
+// let arr = [4, 5, 6, 7]
+// let arr2 = [...arr]
+// let arr3 = arr
+
+
+// arr.push(10)
+// arr2.push(20)
+// arr3.push(30)
+
+// console.log(arr, arr2, arr3);
+// 4 5 6 7 10
+// 4 5 6 7 10 20
+// 4 5 6 7 10 20 30
+
+
+
+
+// let arr = [4, 5, 6, 7, 5, 10]
+
+// let [a, b, c, ...rest] = arr
+// // let a=arr[0]
+// // let b=arr[1]
+// // let c=arr[2]
+// // let d=arr[3]
+
+// console.log(rest);
+
+let data = {
+    name: "node",
+    number: 985769885,
+    price: 498709
+}
+
+
+let { price, name } = data
+
+console.log(price);
