ea29e78223ce62e39416cffc9c58137c171ce239
Rajat sinha <rajatsinha5467@gmail.com>
2024-06-26 11:08:14 +0530

    google

--- NODE/Mvc/middlewares/GoogleAuth.js
+++ NODE/Mvc/middlewares/GoogleAuth.js
@@ -2,8 +2,8 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const googleAuth = (passport) => {
     passport.use(new GoogleStrategy({
-        clientID: "901419004462-87ld1njutdmnfrteo6u0pn8d4meco4mp.apps.googleusercontent.com",
-        clientSecret: "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",
+        clientID: "",
+        clientSecret: "",
         callbackURL: "http://localhost:8090/auth/google/callback"
     },
         function (accessToken, refreshToken, profile, cb) {
