445dac553e797399793d106c3a72fc60e07258ff
Rajat sinha <rajatsinha5467@gmail.com>
2024-07-03 11:13:42 +0530

    mvc

--- NODE/Mvc/Routes/user.route.js
+++ NODE/Mvc/Routes/user.route.js
@@ -7,6 +7,8 @@ const upload = require('../middlewares/uploadImage')
 const { isLoggedIn } = require('../middlewares/user')
 const passport = require('passport')
 const { isLogged } = require('../middlewares/validateUser')
+const { userSignup, userLogin } = require('../controllers/user.Auth.controller')
+const { isVerified } = require('../middlewares/token')
 
 
 // multer
@@ -36,26 +38,35 @@ userRoute.get('/login', loginPage)
 userRoute.post('/login', passport.authenticate("local"), (req, res) => {
     res.send("logged in")
 })
-userRoute.get('/details',isLogged, (req, res) => {
+userRoute.get('/details', isLogged, (req, res) => {
     res.send({ message: "User details", user: req.user })
 })
 
 // mail
-userRoute.post('/mail',sendMail)
+userRoute.post('/mail', sendMail)
 
 
 // reset password
-userRoute.post('/password-reset',isLogged,passwordReset)
-userRoute.get("/password-reset",isLogged,passwordPage)
-
-
-
+userRoute.post('/password-reset', isLogged, passwordReset)
+userRoute.get("/password-reset", isLogged, passwordPage)
 
 
 // password reset by email otp
+userRoute.get("/otp", sendMailPage)
+userRoute.get("/verify", verifyPage)
+userRoute.post("/verify", verifyOtp)
+userRoute.post("/otp", sendOtpByEmail)
 
-userRoute.get("/otp",sendMailPage)
-userRoute.get("/verify",verifyPage)
-userRoute.post("/verify",verifyOtp)
-userRoute.post("/otp",sendOtpByEmail)
+
+// jsonwebToken
+userRoute.post("/signup/token", userSignup)
+userRoute.post("/login/token", userLogin)
+userRoute.get("/isverify", isVerified, (req, res) => {
+    res.send("Welcome to the our website")
+})
+
+
+
+
+
 module.exports = userRoute
